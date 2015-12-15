d699f9d1f46d732a7aac1472a3e40c269b1b320b
zhangzhenlin <664339636@qq.com>
2015-12-15 11:32:41 +0800

    express+react-views

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 var express = require('express');
-
+var path = require('path');
+var reactviews = require('express-react-views');
 var app = express();
+
+var routes = require('./src/routes/index');
+app.use('/', routes);
+//静态资源访问
+app.use(express.static(path.join(__dirname, 'src/output')));
+//设置模板为react
+var options = { beautify: true };//html格式化，默认false
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'jsx');
+app.engine('jsx', reactviews.createEngine(options));
+
+app.listen(3000, function () {
+  console.log('===============Hello Node!===============');//在命令行输出Hello Node!
+});
---
+++ src/views/layouts/default.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+ 
+var DefaultLayout = React.createClass({
+  render: function() {
+    return (
+      <html>
+        <head>
+        	<meta charSet = "UTF-8" />
+        	<title>{this.props.title}</title>
+        </head>
+        <body>
+        	{this.props.children}
+        	<div id="app"></div>
+        	<script src="bundle.js"></script>
+        </body>
+      </html>
+    );
+  }
+});
+ 
+module.exports = DefaultLayout;
